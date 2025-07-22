ab068b391cd784fcf8c6b4d0b56ba85c2185054d
klyzecarize <71118473+klyzecarize@users.noreply.github.com>
2025-07-22 18:18:25 +0800

    interview-2: added momentjs and show all time slots

--- my-booking/src/app/components/slots.js
+++ my-booking/src/app/components/slots.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Slots = () => {
-  let availableSlots = new Date("2025-07-22");
-  availableSlots.setHours(10, 0o0, 0o0)
+const Slots = ({time}) => {
 
   return (
-    <h1>BookSlots {availableSlots.toLocaleTimeString()}</h1>
+    <h1>Time: {time}</h1>
   )
 }
 
--- my-booking/src/app/page.js
+++ my-booking/src/app/page.js
@@ -1,10 +1,33 @@
 import Slots from "./components/slots";
+import moment from "moment";
 
 export default function Home() {
+  let allSlots = GetSlots();
+
+  console.log(allSlots)
+
   return (
     <>
       <h1>Booking</h1>
-      <Slots/>
+      {
+        allSlots.map( (timeSlot, index) => {
+          return <Slots key={index} time={timeSlot} />
+        })
+      }
     </>
   );
+}
+
+function GetSlots () {
+  let slots = [];
+  let currentTime = moment("2025-07-22", "HH:mm");
+  currentTime.set({hour:8, minute:0});
+  slots.push(currentTime.format("hh:mm"));
+
+  for (let i = 0; i <= 17; i++) {
+    currentTime.add({minute:30})
+    slots.push(currentTime.format("hh:mm"));
+  }
+
+  return slots;
 }
