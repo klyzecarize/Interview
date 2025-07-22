2df5ef2a460b6bf758be8578960cbfcbad286895
klyzecarize <71118473+klyzecarize@users.noreply.github.com>
2025-07-22 20:12:36 +0800

    interview-4: code cleaning

--- my-booking/src/app/page.js
+++ my-booking/src/app/page.js
@@ -4,21 +4,20 @@ import moment from "moment";
 export default function Home() {
 
   let currentDate = moment();
-  let allSlots = GetSlots(currentDate);
-  // let bookedSlots = ["10:00", "10:30", "11:00"];
   let bookedSlots = ["21:00", "20:30", "11:00"];
-  allSlots = allSlots
-  .filter((slot) => {
-    if (!bookedSlots.includes(slot.toString())) {
-      return slot >= currentDate.format("HH:mm");
-    }
-  });
-  // allSlots.shift();
+  let allSlots = GetNextAvailableSlot(bookedSlots, currentDate);
 
   return (
     <>
-      <h1>Booking</h1>
-      <h1>Available Slots for {currentDate.format("MM/DD/YYYY")}</h1>
+      <h1>Bookings for {currentDate.format("MM/DD/YYYY")}</h1>
+      <hr/>
+      <h1>Reserved Slots</h1>
+      {
+        bookedSlots.sort().map( (timeSlot, index) => {
+          return <Slots key={index} time={timeSlot} />
+        })
+      }
+      <h1>Available Slots</h1>
       {
         allSlots.map( (timeSlot, index) => {
           return <Slots key={index} time={timeSlot} />
@@ -28,19 +27,23 @@ export default function Home() {
   );
 }
 
-function GetSlots (setTime) {
+function GetNextAvailableSlot (bookings, setDate) {
   let slots = [];
-  let setRemainingHours = (23 - setTime.get('hour')) * 2;
-  let currentTime = setTime;
+  let setRemainingHours = (23 - setDate.get('hour')) * 2;
+  let currentTime = setDate;
   currentTime = moment();
   currentTime.set({minute:0});
 
-  // slots.push(currentTime.format("HH:mm"));
-
   for (let i = 0; i <= setRemainingHours; i++) {
-    currentTime.add({minute:30})
+    currentTime.add({minute:30});
     slots.push(currentTime.format("HH:mm"));
   }
+
+  slots = slots.filter((slot) => {
+    if (!bookings.includes(slot.toString())) {
+      return slot >= setDate.format("HH:mm");
+    }
+  });
 
   return slots;
 }
